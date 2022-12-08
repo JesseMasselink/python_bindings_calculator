cb8865c52678578fe8d6c9b76a41c8adc116f547
JesseMasselink <503042@student.saxion.nl>
2022-12-08 11:58:56 +0100

    "functies toegevoegd"

--- cal_jesse.cpp
+++ cal_jesse.cpp
@@ -6,7 +6,7 @@ float add(float arg1, float arg2){
 }
 // wrapper
 PYBIND11_MODULE(pybind11_add_module m){
-    m.doc() = "pybind11 test function"; // optionele docstring
+    m.doc() = "pybind11 add function"; // optionele docstring
     m.def("cpp_add_function", &add, "A function that adds two numbers");
 }
 
@@ -17,5 +17,26 @@ float multiply(float arg1, float arg2){
 // wrapper
 PYBIND11_MODULE(pybind11_multiply_module, m){
     m.doc() = "pybind11 multiply function"; // optionele docstring
-    m.def("cpp_multiply_function", &multiply, "A function that adds two numbers");
+    m.def("cpp_multiply_function", &multiply, "A function that multiplies two numbers");
+}
+
+// function
+float square(float arg1){
+    return arg1 * arg1;
+}
+// wrapper
+PYBIND11_MODULE(pybind11_square_module, m){
+    m.doc() = "pybind11 square function"; // optionele docstring
+    m.def("cpp_square_function", &square, "A function that gives back the argument squared");
+}
+
+unsigned int fibinacci(int n){
+    if(n < 2){
+        return n;
+    }
+    return fibinacci(n - 1) + fibinacci(n - 2);
+}
+
+PYBIND11_MODULE(pybind11_fibinacci_module, m) {
+    m.def("fibinacci_cpp", &fibinacci, "Recursive fibinacci algorithm.");
 }
